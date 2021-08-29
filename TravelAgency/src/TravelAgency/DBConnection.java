89d8a2edbbd33e7b16b44c738fe76a9e69234622
ermekbaeva <ermekbaevameerim@gmail.com>
2021-08-29 20:50:24 +0300

    added DBConnection file

---
+++ TravelAgency/src/TravelAgency/DBConnection.java
@@ -0,0 +1,34 @@
+package TravelAgency;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnection {
+    private static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String DATABASE_URL = "jdbc:mysql://localhost:3306/travelagency";
+    private static final String USERNAME = "meerim1";
+    private static final String PASSWORD = "123qwe";
+    private static Connection connection;
+
+
+    public static Connection getConnection(){
+        if (connection == null){
+            try{
+                Class.forName(JDBC_DRIVER);
+                connection = DriverManager.getConnection(DATABASE_URL, USERNAME, PASSWORD);
+            } catch (ClassNotFoundException ex){
+                ex.printStackTrace();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+        }
+        return connection;
+    }
+
+
+}
+
